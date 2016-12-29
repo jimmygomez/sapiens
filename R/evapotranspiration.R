4f9e799d606f46eed0f60d893ff9948098c0151c
Flavio <Flavjack>
2016-12-29 16:47:26 -0300

    import packages

---
+++ R/evapotranspiration.R
@@ -0,0 +1,157 @@
+#' FAO56 Penman-Monteith method
+#'
+#' @description function use for calculate the daily evapotranspiration (ET)
+#' @param data data frame with the metrerological information
+#' @param day  date of the weather measure (yyy-mm-dd)
+#' @param Tmin  temperatura minima (ºC)
+#' @param Tmax  temperatura maxima (ºC)
+#' @param RHmin humedad relativa minima (%)
+#' @param RHmax humedad relativa maxima (%)
+#' @param n     sun hours (h)
+#' @param v     wind velocity (m.s-1)
+#' @param lat   latitud en radianes decimales (numeric)
+#' @param alt   altitud (msnm)
+#' @param z     heigth of data collect for metereological station (m)
+#' @param alb   albedo
+#' @return data frame with daily evapotranspiration
+#' @importFrom lubridate yday
+#' @importFrom zoo as.Date
+#' @export
+
+
+PenMon <- function(data, day, Tmin, Tmax, RHmin, RHmax, n, v, lat, alt, 
+    z, alb = 0.23) {
+    
+    
+    data <- as.data.frame(data)
+    
+    day <- zoo::as.Date(data[, day])
+    Tmin <- data[, Tmin]
+    Tmax <- data[, Tmax]
+    RHmin <- data[, RHmin]
+    RHmax <- data[, RHmax]
+    n <- data[, n]
+    v <- data[, v]
+    lat <- data[, lat]
+    alt <- data[, alt]
+    z <- data[, z]
+    
+    
+    # wind <> 2 m
+    # -------------------------------------------------------------
+    
+    # Ec. 47
+    
+    uz <- v * ((4.87)/(log(67.8 * z - 5.42)))
+    
+    
+    # Temperature media
+    # -------------------------------------------------------
+    
+    # Ec. 9
+    
+    Tm <- (Tmax + Tmin)/2
+    
+    # pressure
+    # ----------------------------------------------------------------
+    
+    # Ec. 7
+    
+    P <- 101.3 * ((293 - 0.0065 * alt)/(293))
+    
+    
+    # Constante psicrometrica
+    # -------------------------------------------------
+    
+    # Ec. 8
+    
+    kp <- (1.013 * 10^(-3) * P)/(2.45 * 0.622)
+    
+    
+    # Pendiente de la curva de presion de saturacion de vapor
+    # -----------------
+    
+    # Ec. 13
+    
+    psv <- (4098 * (0.6108 * exp((17.27 * Tm)/(Tm + 237.3))))/(Tm + 237.3)^2
+    
+    
+    # Deficit de presion de vapor
+    # ---------------------------------------------
+    
+    # Ec. 11
+    
+    tx <- 0.6108 * exp((17.27 * Tmax)/(Tmax + 237.3))
+    tm <- 0.6108 * exp((17.27 * Tmin)/(Tmin + 237.3))
+    
+    es <- (tx + tm)/2
+    
+    ea <- (tm * (RHmax/100) + tx * (RHmin/100))/2
+    
+    # Ec. 17
+    
+    dpv <- es - ea
+    
+    
+    # Radiation
+    # ---------------------------------------------------------------
+    
+    
+    J <- lubridate::yday(day)
+    
+    # Ec. 23
+    
+    dr <- 1 + 0.033 * cos((2 * pi/365) * J)
+    
+    # Ec. 24
+    
+    ds <- 0.409 * sin(((2 * pi)/(365)) * J - 1.39)
+    
+    # Ec. 25
+    
+    ws <- acos(-tan(lat) * tan(ds))
+    
+    # Ec. 21
+    
+    Ra <- ((24 * 60)/pi) * 0.082 * dr * (ws * sin(lat) * sin(ds) + cos(lat) * 
+        cos(ds) * sin(ws))
+    
+    
+    # Ec. 34
+    
+    N <- (24/pi) * ws
+    
+    # Ec. 35
+    
+    Rs <- (0.25 + 0.5 * (n/N)) * Ra
+    
+    # Ec. 37
+    
+    Rso <- (0.75 + 2 * 10^(-5) * alt) * Ra
+    
+    # Ec. 38
+    
+    Rns <- (1 - alb) * Rs
+    
+    # Ec. 39
+    
+    Rnl <- ((4.903 * 10^(-9)) * (((Tmax + 273.16)^4 + (Tmin + 273.16)^4)/2)) * 
+        (0.34 - 0.14 * sqrt(ea)) * (1.35 * (Rs/Rso) - 0.35)
+    
+    # Ec. 40
+    
+    Rn <- Rns - Rnl
+    
+    # Ec. 42 Gday = 0
+    
+    a <- psv/(psv + kp * (1 + 0.34 * uz))
+    
+    b <- kp/(psv + kp * (1 + 0.34 * uz))
+    
+    c <- (900/(Tm + 273)) * uz
+    
+    
+    et <- 0.408 * Rn * a + c * dpv * b
+    
+    
+}
--- R/runapp.R
+++ R/runapp.R
@@ -6,22 +6,24 @@
 #' @export
 
 elisios <- function() {
-  appDir <- system.file("elisios", package = "sapiens")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `sapiens`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
+    appDir <- system.file("elisios", package = "sapiens")
+    if (appDir == "") {
+        stop("Could not find example directory. Try re-installing `sapiens`.", 
+            call. = FALSE)
+    }
+    
+    shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
 }
 
 
 wanuy <- function() {
-  appDir <- system.file("wanuy", package = "sapiens")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `sapiens`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
+    appDir <- system.file("wanuy", package = "sapiens")
+    if (appDir == "") {
+        stop("Could not find example directory. Try re-installing `sapiens`.", 
+            call. = FALSE)
+    }
+    
+    shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
 }
 
 
